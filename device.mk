92bdfedf5c856931c8094e4b65cdfcb0b4fcb84d
LuK1337 <priv.luk@gmail.com>
2024-08-21 23:47:56 +0100

    Tetris: Use libcamera_metadata_shim for unlocking cameras
    Change-Id: I985c6e317b36d8cdc6b41a9f9f625b293a8987eb

--- device.mk
+++ device.mk
@@ -196,7 +196,8 @@ PRODUCT_PACKAGES += \
 # Graphics shims
 PRODUCT_PACKAGES += \
     libprocessgroup_shim \
-    libbase_shim
+    libbase_shim \
+    libcamera_metadata_shim
 
 # DRM
 PRODUCT_PACKAGES += \
