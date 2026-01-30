3b2797a1f4d0256e72d19a4bb5352bc5faf80f0c
Elias Ethan Gheeraert <eliasgheeraert@gmail.com>
2026-01-30 22:37:47 +1100

    Tetris: Build mediaTek WiFi lib and enable OUI support on wpa_supplicant_8

--- device.mk
+++ device.mk
@@ -429,10 +429,11 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/pixel \
-    hardware/google/interfaces \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/wlan/wifi_hal \
+    hardware/google/pixel \
+    hardware/google/interfaces \
     $(LOCAL_PATH)
 
 # USB
@@ -446,6 +447,8 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.Tetris
 
 # Wifi
+$(call soong_config_set,wpa_supplicant_8,board_wlan_mediatek_stability,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload-V1-ndk.vendor \
     android.hardware.wifi-V1-ndk.vendor \
@@ -456,7 +459,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libkeystore-wifi-hidl \
     libkeystore-engine-wifi-hidl \
-    libwifi-hal-wrapper \
     libnetutils \
     libnetutils.vendor \
     wifi_legacy \
