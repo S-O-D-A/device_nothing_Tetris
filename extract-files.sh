69572c95f8b6cc4b5a12cd079cb2e74a57b5e3ed
abdulla-li <abdullashakkeeb693@gmail.com>
2026-04-15 14:01:32 +0530

    Revert "Tetris: init: Use foreground UClamp cpusets for our composer service too"
    This reverts commit e299fef16a35a1361dad54b1d58b602a9c7f1bdb.

--- extract-files.sh
+++ extract-files.sh
@@ -71,23 +71,15 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.graphics.allocator@4.0-service-mediatek.rc)
             sed -i 's|android.hardware.graphics.allocator@4.0-service-mediatek|mt6878/android.hardware.graphics.allocator@4.0-service-mediatek.mt6878|g' "${2}"
             ;;
+        vendor/etc/init/android.hardware.graphics.allocator-V2-service-mediatek.rc)
+            sed -i 's|android.hardware.graphics.allocator-V2-service-mediatek|mt6878/android.hardware.graphics.allocator-V2-service-mediatek.mt6878|g' "${2}"
+            ;;
         vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc)
             sed -i 's|start|enable|g' "$2"
             ;;
         vendor/lib64/hw/vendor.mediatek.hardware.pq_aidl-impl.so)
             "$PATCHELF" --replace-needed "libui.so" "libui-v34.so" "$2"
             "$PATCHELF" --replace-needed "libtinyxml2.so" "libtinyxml2-v34.so" "$2"
-            ;;
-            vendor/etc/init/android.hardware.graphics.allocator-V2-service-mediatek.rc)
-            sed -i 's|android.hardware.graphics.allocator-V2-service-mediatek|mt6878/android.hardware.graphics.allocator-V2-service-mediatek.mt6878|g' "${2}"
-            sed -i '/task_profiles ServiceCapacityLow/d' "${2}"
-            sed -i '/task_profiles ProcessCapacityHigh HighPerformance/d' "${2}"
-            sed -i '/class hal/a \    task_profiles ProcessCapacityHigh HighPerformance' "${2}"
-            ;;
-        vendor/etc/init/android.hardware.graphics.composer@3.2-service.rc)
-            sed -i '/task_profiles ServiceCapacityLow/d' "${2}"
-            sed -i '/task_profiles ProcessCapacityHigh HighPerformance/d' "${2}"
-            sed -i '/class hal/a \    task_profiles ProcessCapacityHigh HighPerformance' "${2}"
             ;;
         vendor/lib64/libnvram.so)
             "$PATCHELF" --add-needed libbase_shim.so "$2"
