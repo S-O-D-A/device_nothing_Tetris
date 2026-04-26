50b65004e95806e30d2e9670927924099952e6d7
beingashwani <ashwanic177@gmail.com>
2026-04-26 21:48:49 +0530

    Tetris: tune LMK parameters for improved memory handling

--- vendor_props.mk
+++ vendor_props.mk
@@ -279,12 +279,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.downgrade_pressure=60 \
     ro.lmk.filecache_min_kb=153600 \
-    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.kill_timeout_ms=100 \
     ro.lmk.stall_limit_critical=40 \
     ro.lmk.psi_complete_stall_ms=700 \
     ro.lmk.swap_free_low_percentage=10 \
-    ro.lmk.swap_util_max=100 \
-    ro.lmk.thrashing_limit=100 \
+    ro.lmk.swap_util_max=90 \
+    ro.lmk.thrashing_limit=40 \
     ro.lmk.thrashing_limit_decay=10 \
     ro.logd.kernel=false
 
